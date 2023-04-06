7c8205a1574280362542d7b93551ea1d89f1ce1e
Epic <EpicEbic@outlook.com>
2023-04-06 08:47:50 +0100

    remove unused import

--- src/main/java/me/epic/chatgames/storage/SQLiteStorageHandler.java
+++ src/main/java/me/epic/chatgames/storage/SQLiteStorageHandler.java
@@ -10,7 +10,6 @@ import org.bukkit.OfflinePlayer;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
