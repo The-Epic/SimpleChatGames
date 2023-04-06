40ca0903fb58791cc2de27df5537255270259172
Epic <EpicEbic@outlook.com>
2023-04-06 18:32:53 +0100

    mysql is painful

--- src/main/java/me/epic/chatgames/storage/StorageHandler.java
+++ src/main/java/me/epic/chatgames/storage/StorageHandler.java
@@ -1,9 +1,7 @@
 package me.epic.chatgames.storage;
 
-import lombok.SneakyThrows;
 import me.epic.chatgames.utils.PlayerData;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Player;
 
 import java.util.List;
 
